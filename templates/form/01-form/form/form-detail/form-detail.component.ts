eb1baa64216c90cc78d7162e5e63fe9989d8e0d5
nprol <134269709+nprol@users.noreply.github.com>
2025-07-29 12:56:32 +0200

    Add form template 01 (#22)
    * Form template 01 created
    
    * Modify avatar image and added readme
    
    * Depuring theme
    
    * Changes made
    
    * Attrs added on inputs, recalc % and margins
    
    * README updated
    
    * design changes made
    
    * change photo type
    
    ---------
    
    Co-authored-by: Noelia Prol Carballo <noelia.prol@imatia.com>

---
+++ templates/form/01-form/form/form-detail/form-detail.component.ts
@@ -0,0 +1,69 @@
+import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import { OFormComponent } from 'ontimize-web-ngx';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-form-detail',
+  templateUrl: './form-detail.component.html',
+  styleUrls: ['./form-detail.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+
+export class FormDetailComponent {
+
+  @ViewChild('form') form: OFormComponent;
+
+  public name: string;
+  public surname: string;
+  public address: string;
+  public phone: string;
+  public id: number;
+  public photo: string;
+  public show: boolean;
+  public icon: string;
+
+  constructor(protected sanitizer: DomSanitizer) {
+    this.icon = "keyboard_arrow_up";
+    this.show = true;
+  }
+
+  showInfo(evt: any) {
+    this.show = !this.show;
+    if (this.show) {
+      this.icon = "keyboard_arrow_up";
+    } else {
+      this.icon = "keyboard_arrow_down";
+    }
+  }
+
+  onFormDataLoaded(data: any) {
+    if (data.NAME) {
+      this.name = data.NAME;
+    }
+
+    if (data.SURNAME) {
+      this.surname = data.SURNAME;
+    }
+
+    if (data.ADDRESS) {
+      this.address = data.ADDRESS;
+    }
+
+    if (data.PHONE) {
+      this.phone = data.PHONE;
+    }
+
+    if (data.CUSTOMERID) {
+      this.id = data.CUSTOMERID;
+    }
+
+    if (data.PHOTO) {
+      this.photo = data.PHOTO;
+    }
+  }
+
+  public getImageSrc(base64: any): any {
+    return base64 ? this.sanitizer.bypassSecurityTrustResourceUrl('data:image/*;base64,' + base64) : './assets/images/no-image.png';
+  }
+
+}
