501605ddb6dbb10168ef406b7820f44dc5d234ed
javier-fernandez-imatia <164156437+javier-fernandez-imatia@users.noreply.github.com>
2025-10-06 09:19:50 +0200

    Table template 02 created (#20)
    * Table template 02 created
    
    * Added icon registry file and app finished
    
    * Card size changed
    
    * Requested changes made
    
    * Update templates/table/02-table/README.md
    
    Co-authored-by: Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
    
    * Update templates/table/02-table/table/table-home/table-home.component.ts
    
    Co-authored-by: Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
    
    * Readme updated
    
    * changes requested made
    
    * renderer action no resizable and max-width
    
    * Update templates/table/02-table/table/table-home/table-home.component.html
    
    Co-authored-by: Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
    
    * Update templates/table/02-table/README.md
    
    Co-authored-by: Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
    
    * Update templates/table/02-table/README.md
    
    Co-authored-by: Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
    
    * O-filter-builder explanation modified, change bg-level on theme, add new class to div bg-level-1 in html and modified in scss
    
    * Cards style modified
    
    * Standard styling applied and card styles in dark mode refined
    
    * modified color in text and icon inside cards
    
    ---------
    
    Co-authored-by: Noelia Prol Carballo <noelia.prol@imatia.com>
    Co-authored-by: nprol <134269709+nprol@users.noreply.github.com>
    Co-authored-by: Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>

---
+++ templates/table/02-table/table/table-home/table-home.component.ts
@@ -0,0 +1,104 @@
+import { Component, Injector, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Expression, FilterExpressionUtils, OFilterBuilderComponent, OntimizeService, OTableButtonComponent, OTableComponent } from 'ontimize-web-ngx';
+import { forkJoin } from 'rxjs';
+import { Type } from 'src/app/shared/interfaces/type.interface';
+
+@Component({
+  selector: 'app-table-home',
+  templateUrl: './table-home.component.html',
+  styleUrls: ['./table-home.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+
+export class TableHomeComponent implements OnInit {
+
+  @ViewChild('filterBuilder', { static: false }) filterBuilder: OFilterBuilderComponent;
+  @ViewChild('filterButton', { static: false }) filterButton: OTableButtonComponent;
+
+  public selectedType = 0;
+  types: Type[] = [];
+  private service: OntimizeService;
+  public filtersOpen = false;
+
+  private iconMap: Record<number, string> = {
+    1: 'ontimize:normal',
+    2: 'ontimize:VIP',
+    3: 'ontimize:other'
+  };
+
+  constructor(protected injector: Injector) {
+    this.service = this.injector.get(OntimizeService);
+  }
+
+  ngOnInit() {
+    this.configureService();
+
+    const dataTypes = this.service.query({}, ["CUSTOMERTYPEID", "DESCRIPTION"], "customerType");
+
+    const dataCustomers = this.service.query({}, ['CUSTOMERTYPEID'], 'customer');
+
+    forkJoin({ cusTypes: dataTypes, customers: dataCustomers }).subscribe(({ cusTypes, customers }) => {
+      const counts: Record<number, number> = {};
+      customers.data.forEach((row: any) => {
+        const id = row.CUSTOMERTYPEID;
+        counts[id] = (counts[id] ?? 0) + 1;
+      });
+
+      this.types = cusTypes.data.map((t: any) => ({
+        id: t.CUSTOMERTYPEID,
+        label: t.DESCRIPTION,
+        icon: this.iconMap[t.CUSTOMERTYPEID] ?? 'ontimize:unknown',
+        count: counts[t.CUSTOMERTYPEID] ?? 0
+      }));
+
+      this.types.push({
+        id: 0,
+        label: 'ALL',
+        icon: 'ontimize:all',
+        count: customers.data.length
+      });
+
+    });
+  }
+
+  ngAfterViewInit() {
+    this.filterButton.onClick.subscribe(event => {
+      this.filtersOpen = !this.filtersOpen;
+    })
+  }
+
+  protected configureService() {
+    // Configure the service using the configuration defined in the `app.services.config.ts` file
+    const conf = this.service.getDefaultServiceConfiguration('customers');
+    this.service.configureService(conf);
+  }
+
+  onSelect(id: number) {
+    this.selectedType = id;
+    this.filterBuilder?.triggerReload();
+  }
+
+  trackById(index: number, item: { id: number }) {
+    return item.id;
+  }
+
+  createFilter = (values: Array<{ attr, value }>): Expression => {
+    let filters: Array<Expression> = [];
+    if (this.selectedType !== 0) {
+      filters.push(FilterExpressionUtils.buildExpressionEquals("CUSTOMERTYPEID", this.selectedType));
+    }
+
+    values.forEach(fil => {
+      if (fil.value) {
+        if (fil.attr === 'NAME' || fil.attr === 'SURNAME' || fil.attr === 'EMAIL') {
+          filters.push(FilterExpressionUtils.buildExpressionLike(fil.attr, fil.value));
+        }
+      }
+    });
+
+    return filters.length > 0
+      ? filters.reduce((exp1, exp2) =>
+        FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_AND))
+      : null;
+  }
+}
---
+++ templates/table/02-table/table/table-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TableHomeComponent } from './table-home/table-home.component';
+
+const routes: Routes = [
+  { path: '', component: TableHomeComponent },
+  { path: ':CUSTOMERID', component: TableHomeComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TableRoutingModule { }
